b3e117010c7caa4f80cc734a0a4ca28d80e06077
Lvov Alexandr <strangeisdoctor@gmail.com>
2024-08-09 17:15:16 +0300

    cosmetic changes

--- main.go
+++ main.go
@@ -1,20 +1,15 @@
 package main
 
 import (
-	"anime/database"
-	"anime/router"
+	"log"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/gofiber/template/html/v2"
-	"log"
-)
 
-type Anime struct {
-	ID          uint64 `json:"mal_id"`
-	Title       string `json:"title"`
-	Episodes    uint16 `json:"episodes"`
-	Description string `json:"synopsis"`
-}
+	"anime/database"
+	"anime/router"
+)
 
 func main() {
 
@@ -24,14 +19,18 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	database.CreateTables(conn)
-	//database.AddAllAnimes()
 	defer conn.Close()
+
+	database.CreateTables(conn)
+
+	//database.AddAllAnimes() # uncomment if table is not filled
 
 	app := fiber.New(fiber.Config{
 		Views: engine,
 	})
+
 	app.Use(logger.New())
+	
 	router.SetupRoutes(app)
 
 	log.Fatal(app.Listen(":3000"))
